310acef000eaa16f4611af770cb7d4b8d1121d22
Ahmad <ahmadhijazi_me@hotmail.com>
2025-10-04 19:32:29 +0200

    Pipeline Test

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {
-  private y = 1;
-  protected x = 1;
-}
+export class HomePage {}
